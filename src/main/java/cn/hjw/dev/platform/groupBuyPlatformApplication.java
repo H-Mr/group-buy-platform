2ed634b2482fb77e49167f46660645f8f54990ad
hjw <487803149@qq.com>
2025-12-18 19:51:04 +0800

    feat:完成CICD

--- src/main/java/cn/hjw/dev/platform/groupBuyPlatformApplication.java
+++ src/main/java/cn/hjw/dev/platform/groupBuyPlatformApplication.java
@@ -1,5 +1,6 @@
 package cn.hjw.dev.platform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling //开启定时任务
  public class groupBuyPlatformApplication {
   public static void main(String[] args) {
+
+   // 加载根目录的 .env 文件到系统环境变量
+   Dotenv dotenv = Dotenv.load();
+   dotenv.entries().forEach(entry ->
+           System.setProperty(entry.getKey(), entry.getValue())
+   );
    SpringApplication.run(groupBuyPlatformApplication.class, args);
   }
  }
